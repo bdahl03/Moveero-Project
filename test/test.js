c68b2bee5bacd28d9e21e3974bda3825deb85035
Dahl <46387446+bdahl03@users.noreply.github.com>
2023-02-02 15:41:29 -0600

    removed temporary test files

--- test/test.js
+++
@@ -1,9 +0,0 @@
-function testFunction(){
-    alert("Test");
-}
-
-function getValue() {
-    var x = document.getElementById("valueTest").value;
-    // alert(x);
-    document.getElementById("output").innerHTML = x
-}
