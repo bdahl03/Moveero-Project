733e806c3e0085d4d13c82c1f7db376cd5979321
Dahl <46387446+bdahl03@users.noreply.github.com>
2023-02-08 12:09:12 -0600

    Working tables
    Fully working Bolt Hole Table and more tables

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 
 function updateTables() {
     const num_holes = parseInt(document.getElementById("NumHoles").value)
-    var bolt_hole_table = document.getElementById("boltHoleTable")
 
-    updateTableSize(bolt_hole_table, num_holes, 3)
-
-
+    //====bolt hole table====
+    var bolt_hole_table = document.getElementById("boltHoleTable") // gets table
+    updateTableSize(bolt_hole_table, num_holes, 4)
+    var table_rows = getHTMLTableRows(bolt_hole_table)
     // writeOutputsTableRepeat(0, bolt_hole_table, input_obj)
-    for (var i = 0, row; row = bolt_hole_table.rows[i]; i++) {
-        row.cells[0].appendChild(createNumberInputObject())
-        row.cells[1].innerHTML = "0.0"
-        row.cells[2].innerHTML = "None"
+    for (var i = 0, row; row = table_rows[i]; i++) {
+        row.cells[0].innerHTML = i + 1
+        row.cells[0].style.textAlign = "center"
+
+        row.cells[1].appendChild(createNumberInputObject())
+
+        row.cells[2].innerHTML = "0.0"
+
+        row.cells[3].innerHTML = "None"
     }
+    //====
+    
 }
 
 function updateTableSize(table, num_rows, num_cols) {
-    removeAllTableRows(table)
+    // removeAllTableRows(table)
+    tbody = table.getElementsByTagName('tbody')[0]
+    table.removeChild(table.getElementsByTagName('tbody')[0])
+    table_body = table.appendChild(document.createElement("TBODY"))
+    // console.log(table_body)
+
     for (var i = 0; i < num_rows; i++) {
-        row = table.insertRow()
+        row = table_body.insertRow()
         for (var j = 0; j < num_cols; j++) {
             cell = row.insertCell()
         }
     }
 }
 
-function removeAllTableRows(table) {
-    var len = table.rows.length
-    // console.log(len)
-    for (var i = 0; i < len; i++) {
-        table.deleteRow(0)
-    }
-}
+// unnecessary use this instead:
+// tbody = table.getElementsByTagName('tbody')[0]
+// table.removeChild(tbody)
+
+// function removeAllTableRows(table) {
+//     var len = table.rows.length
+//     // console.log(len)
+//     for (var i = 0; i < len; i++) {
+//         table.deleteRow(0)
+//     }
+// }
 
 function calculate() {
 
@@ -40,9 +56,9 @@ function calculate() {
     const min = bolt_hole_dia - lower_tol
     const max = bolt_hole_dia + upper_tol
 
-    var input_index = 0
-    var tolerance_output_index = 1
-    var ok_output_index = 2
+    var input_index = 1
+    var tolerance_output_index = 2
+    var ok_output_index = 3
     var table = document.getElementById("boltHoleTable")
     holes = getInputsTable(input_index, table)
     // console.log(holes)
@@ -56,7 +72,8 @@ function calculate() {
 // abs(hole-avg(max,min)) / ((max-min) / 2)
 function doCheckTolerance(output_index, table, holes, min, max) {
     var outputs = []
-    for (var i = 0; i < table.rows.length; i++) {
+    var len = getHTMLTableRows(table).length
+    for (var i = 0; i < len; i++) {
         outputs.push(calculateTolerance(holes[i], min, max))
     }
     writeOutputsTable(output_index, table, outputs)
@@ -67,7 +84,7 @@ function calculateTolerance(hole, min, max) {
 }
 
 // =IF(B22<=$G$7,IF(B22>=$H$7,"OK","NOK"),"NOK")
-// 
+
 function doCheckOk(output_index, table, holes, min, max) {
     var outputs = []
     for (var i = 0; i < table.rows.length; i++) {
@@ -85,6 +102,8 @@ function calculateOk(hole, min, max) {
     else {return "NOK"}
 
 }
+//====
+
 
 function getInputsTable(input_index, table, row_min = 0) {
     // input_index: the column for inputs
@@ -92,7 +111,8 @@ function getInputsTable(input_index, table, row_min = 0) {
     var input_list = []
 
     // loop though rows and cols of the table
-    for (var i = row_min, row; row = table.rows[i]; i++) {
+    table_rows = getHTMLTableRows(table)
+    for (var i = row_min, row; row = table_rows[i]; i++) {
 
         col_val = parseFloat(row.cells[input_index].firstChild.value)
         input_list.push(col_val)
@@ -102,16 +122,17 @@ function getInputsTable(input_index, table, row_min = 0) {
 }
 
 function writeOutputsTable(output_index, table, outputs) {
-    for (var i = 0, row; row = table.rows[i]; i++) {
+    table_rows = getHTMLTableRows(table)
+    for (var i = 0, row; row = table_rows[i]; i++) {
         row.cells[output_index].innerHTML = outputs[i]
     }
 }
 
-function writeOutputsTableRepeat(output_index, table, output) {
-    for (var i = 0, row; row = table.rows[i]; i++) {
-        row.cells[output_index].innerHTML = output
-    }
-}
+// function writeOutputsTableRepeat(output_index, table, output) {
+//     for (var i = 0, row; row = table.rows[i]; i++) {
+//         row.cells[output_index].innerHTML = output
+//     }
+// }
 
 function createNumberInputObject() {
     // <input type="number" step="any" min="0" value="0.8360">
@@ -121,4 +142,10 @@ function createNumberInputObject() {
     input_obj.setAttribute("min", "0")
     input_obj.setAttribute("value", "0.8360")
     return input_obj
+}
+
+function getHTMLTableRows(table) {
+    var table_body = table.getElementsByTagName('tbody')[0]
+    var table_rows = table_body.getElementsByTagName('tr')
+    return table_rows
 }
